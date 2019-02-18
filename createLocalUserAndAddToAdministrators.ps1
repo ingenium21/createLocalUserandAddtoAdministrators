cc4a89183d0bdddbf062c7463ecf75c65286da30
Renato Regalado <renato.regalado@gmail.com>
2019-02-18 11:13:46 -0600

    Initial commit

---
+++ createLocalUserAndAddToAdministrators.ps1
@@ -0,0 +1,10 @@
+$Password = Read-host -asSecureString "Enter the Password:" 
+
+#get list of computers
+$servers = get-content .\servers.txt
+
+
+foreach ($s in $servers) {invoke-command -computerName $s -scriptBlock {
+New-LocalUser "ansi" -Password $Password -FullName "ansi" -Description "this user is created for ansible" -AccountNeverExpires -PasswordNeverExpires;
+Add-LocalGroupMember -
+}}
