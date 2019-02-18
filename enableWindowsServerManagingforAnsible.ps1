b1fca85d89260ecbb96431db09ea7637d9d10a10
Renato Regalado <renato.regalado@gmail.com>
2019-02-18 13:08:55 -0600

    Create enableWindowsServerManagingforAnsible.ps1

---
+++ enableWindowsServerManagingforAnsible.ps1
@@ -0,0 +1,6 @@
+﻿#get list of computers
+$servers = get-content .\servers.txt
+
+foreach ($s in $servers){
+.\PsExec.exe \\$s -accepteula -nobanner -s powershell -ExecutionPolicy Bypass -Command "iwr https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -UseBasicParsing | iex"
+}
